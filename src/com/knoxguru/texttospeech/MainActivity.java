10120736599618612a41955dfdec5fc1c61b8bbd
knoxguru <knoxguru@gmail.com>
2014-04-03 10:36:25 -0400

    Removed unneeded permissions

--- src/com/knoxguru/texttospeech/MainActivity.java
+++ src/com/knoxguru/texttospeech/MainActivity.java
@@ -15,25 +15,25 @@ import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.Toast;
 
-public class MainActivity extends Activity implements OnCheckedChangeListener {
+public class MainActivity extends Activity {
 
 	public static boolean SERVICE_STATUS;
 	public static boolean AUTO_PLAY = false;
 	public static final String PREFS_NAME = "ttsPrefsFile";
 	SharedPreferences settings;
 
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		View view = getWindow().getDecorView().findViewById(android.R.id.content);
+		View view = getWindow().getDecorView().findViewById(
+				android.R.id.content);
 		disableBroadcastReceiver(view);
 		enableBroadcastReceiver(view);
 		settings = getSharedPreferences(PREFS_NAME, 0);
 		toggleAutoStop(settings.getBoolean("AUTO_PLAY", false));
-		
+
 		Button b = (Button) findViewById(R.id.end_btn);
 
 		if (SERVICE_STATUS == false)
@@ -43,11 +43,12 @@ public class MainActivity extends Activity implements OnCheckedChangeListener {
 
 			@Override
 			public void onClick(View v) {
-				Button b = (Button) v.findViewById(R.id.button1);
+				Button b = (Button) v.findViewById(R.id.end_btn);
 				if (b.getText().toString() == "End Service") {
 					disableBroadcastReceiver(v);
 					b.setText("Start Service");
-					Toast.makeText(getApplicationContext(), "Service Stopped", Toast.LENGTH_SHORT).show();
+					Toast.makeText(getApplicationContext(), "Service Stopped",
+							Toast.LENGTH_SHORT).show();
 				} else {
 					enableBroadcastReceiver(v);
 					b.setText("End Service");
@@ -57,24 +58,25 @@ public class MainActivity extends Activity implements OnCheckedChangeListener {
 
 		CheckBox cb = (CheckBox) findViewById(R.id.btn_auto_play);
 		cb.setChecked(settings.getBoolean("AUTO_PLAY", false));
-		
+
 		cb.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 
 			@Override
 			public void onCheckedChanged(CompoundButton buttonView,
 					boolean isChecked) {
-						toggleAutoStop(isChecked);
+				toggleAutoStop(isChecked);
 			}
 		});
 	}
-	
+
 	public void toggleAutoStop(boolean a) {
 		SharedPreferences.Editor editor = settings.edit();
 		editor.putBoolean("AUTO_PLAY", a);
 		editor.commit();
-		Toast.makeText(getApplicationContext(), "Changed Auto Play setting", Toast.LENGTH_SHORT).show();
+		Toast.makeText(getApplicationContext(), "Changed Auto Play setting",
+				Toast.LENGTH_SHORT).show();
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		return true;
@@ -88,25 +90,21 @@ public class MainActivity extends Activity implements OnCheckedChangeListener {
 		pm.setComponentEnabledSetting(receiver,
 				PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
 				PackageManager.DONT_KILL_APP);
-		Toast.makeText(getApplicationContext(), "Service Started", Toast.LENGTH_SHORT).show();
+		Toast.makeText(getApplicationContext(), "Service Started",
+				Toast.LENGTH_SHORT).show();
 	}
 
-	public void disableBroadcastReceiver(View view){
+	public void disableBroadcastReceiver(View view) {
 		SERVICE_STATUS = false;
-		try{
-	    ComponentName receiver = new ComponentName(this, SMSReceiver.class);
-	    PackageManager pm = this.getPackageManager();
-	        pm.setComponentEnabledSetting(receiver,
-	            PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
-	            PackageManager.DONT_KILL_APP);
-		} catch(Exception e) {
+		try {
+			ComponentName receiver = new ComponentName(this, SMSReceiver.class);
+			PackageManager pm = this.getPackageManager();
+			pm.setComponentEnabledSetting(receiver,
+					PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+					PackageManager.DONT_KILL_APP);
+		} catch (Exception e) {
 			return;
 		}
-	   }
-
-	@Override
-	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-		// TODO Auto-generated method stub
-
 	}
+
 }
--- src/com/knoxguru/texttospeech/Receivers/SMSReceiver.java
+++ src/com/knoxguru/texttospeech/Receivers/SMSReceiver.java
@@ -1,19 +1,19 @@
 package com.knoxguru.texttospeech.Receivers;
 
-
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.telephony.SmsMessage;
-
 
 public class SMSReceiver extends BroadcastReceiver {
 
 	private static final String ACTION_SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
 	public static boolean START_STICKY = false;
 	private Intent mIntent;
-	private String txtText ="";
-	
+	private String txtText = "";
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
@@ -27,23 +27,48 @@ public class SMSReceiver extends BroadcastReceiver {
 			SmsMessage[] msgs = getMessagesFromIntent(mIntent);
 			if (msgs != null) {
 				for (int i = 0; i < msgs.length; i++) {
-					String l = msgs[i].getMessageBody().toString();
-					if (l != null || l != "null") 
-						txtText += msgs[i].getMessageBody().toString() + "\n";
+					txtText += "Message From "
+							+ getContactName(context, msgs[i]
+									.getOriginatingAddress().toString()) + "\n";
+					txtText += msgs[i].getMessageBody().toString() + "\n";
 				}
 			} else {
 				txtText = "Could not get message to read";
 			}
-			
+
 			Intent i = new Intent();
-			i.setClassName("com.knoxguru.texttospeech", "com.knoxguru.texttospeech.ReadActivity");
+			i.setClassName("com.knoxguru.texttospeech",
+					"com.knoxguru.texttospeech.ReadActivity");
 			i.setAction("SMS_RECEIVED_ACTION");
-			i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);  
+			i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 			i.putExtra("sms", txtText);
 			context.startActivity(i);
-			
+
 		}
 
+	}
+
+	private String getContactName(Context context, String number) {
+
+	    String name = null;
+
+	    String[] projection = new String[] {
+	            ContactsContract.PhoneLookup.DISPLAY_NAME,
+	            ContactsContract.PhoneLookup._ID};
+
+	    Uri contactUri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(number));
+
+	    Cursor cursor = context.getContentResolver().query(contactUri, projection, null, null, null);
+
+	    if(cursor != null) {
+	        if (cursor.moveToFirst()) {
+	            name =      cursor.getString(cursor.getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME));
+	        } else {
+	            return number.replace("", " ").trim();
+	        }
+	        cursor.close();
+	    }
+	    return name;
 	}
 
 	public static SmsMessage[] getMessagesFromIntent(Intent intent) {
